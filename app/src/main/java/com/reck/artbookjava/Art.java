74fee73c5f3d7af78b746111506b2ec81be56659
salihkertik <salihkertik1453@gmail.com>
2022-12-02 04:32:34 +0300

    Initial commit

---
+++ app/src/main/java/com/reck/artbookjava/Art.java
@@ -0,0 +1,12 @@
+package com.reck.artbookjava;
+
+public class Art {
+
+    String name;
+    int id;
+
+    public Art(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
+}
